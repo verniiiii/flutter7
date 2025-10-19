a1c8e70b4fd4d868134866c5f7fd5e11acb1bb0e
verniiiii <vera.shestopalova@gmail.com>
2025-10-19 12:49:59 +0300

    create structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'package:go_router/go_router.dart';
+import 'core/app_router.dart';
 
 void main() {
-  runApp(const App());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  final GoRouter _router = AppRouter().router;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: _router,
+      title: 'Финансовый Трекер',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
